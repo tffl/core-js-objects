55bc1667ed2dac2e0473607a86eb52447d0352f9
ntsh <106670334+tffl@users.noreply.github.com>
2024-03-12 18:24:45 +0300

    feat: makeImmutable solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -113,8 +113,9 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  const immObj = Object.freeze(obj);
+  return immObj;
 }
 
 /**
